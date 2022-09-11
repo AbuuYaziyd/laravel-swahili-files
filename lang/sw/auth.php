09f3f0f12ac0bf57220b05506d6583077977fe16
Abou Yaziyd <gakibrush@gmail.com>
2022-09-11 23:21:18 +0300

    auth

--- lang/sw/auth.php
+++ lang/sw/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Hati hizi hazilingani na rekodi zetu.',
+    'password' => 'Nywila ilotolewa si sahihi.',
+    'throttle' => 'Majaribio mengi sana ya kuingia. Tafadhali jaribu tena baada ya :seconds.',
 
 ];
