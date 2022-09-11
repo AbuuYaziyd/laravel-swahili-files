71d3c35d9f40ef0fb09eaf91e44a415053b07924
Abou Yaziyd <gakibrush@gmail.com>
2022-09-11 23:27:52 +0300

    validation

--- lang/sw/validation.php
+++ lang/sw/validation.php
@@ -13,12 +13,12 @@ return [
     |
     */
 
-    'accepted' => 'The :attribute must be accepted.',
-    'accepted_if' => 'The :attribute must be accepted when :other is :value.',
-    'active_url' => 'The :attribute is not a valid URL.',
-    'after' => 'The :attribute must be a date after :date.',
-    'after_or_equal' => 'The :attribute must be a date after or equal to :date.',
-    'alpha' => 'The :attribute must only contain letters.',
+    'accepted' => ':attribute lazima ukubaliwe.',
+    'accepted_if' => ':attribute lazima ikubaliwe wakati :other ni :value.',
+    'active_url' => ':attribute si URL halali.',
+    'after' => ':attribute lazima iwe tarehe baada ya :date.',
+    'after_or_equal' => ':attribute lazima iwe tarehe baada au sawa na :date.',
+    'alpha' => ':attribute lazima iwe na herufi pekee.',
     'alpha_dash' => 'The :attribute must only contain letters, numbers, dashes and underscores.',
     'alpha_num' => 'The :attribute must only contain letters and numbers.',
     'array' => 'The :attribute must be an array.',
